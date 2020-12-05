05bcb576453f23b8a075b76e2c73994955078b99
Wael <dodge_9@hotmail.com>
2020-12-05 18:34:49 -0500

    AddContact()

--- Console_App/ContactHandler.cs
+++ Console_App/ContactHandler.cs
@@ -1,36 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Console_App
-{
-    public class ContactHandler
-    {
+namespace Console_App {
+    public class ContactHandler {
         string ConString = ConfigurationManager.ConnectionStrings["ContactConn"].ConnectionString;
-        
 
-        public void AddContact(Contact contact) 
-        {
-            
+        public void AddContact(Contact contact) {
+            using (SqlConnection con = new SqlConnection(ConString)) {
+                con.Open();
+
+                string query = "INSERT INTO ContactList(Name, Phone_Number, Address, Birthday) VALUES (@Name, @Phone_Number, @Address, @Birthday)";
+
+                SqlCommand command = new SqlCommand(query, con);
+
+                command.Parameters.AddWithValue("@Name", contact.Name);
+                command.Parameters.AddWithValue("@Phone_Number", contact.PhoneNumber);
+                command.Parameters.AddWithValue("@Address", contact.Address);
+                command.Parameters.AddWithValue("@Birthday", contact.Birthday);
+
+                int count = command.ExecuteNonQuery();
+                if (count == 1)
+                    Console.WriteLine("Successfully added new contact!");
+                else
+                    Console.WriteLine("ERROR: Couldn't add new contact!");
+
+                con.Close();
+            }
         }
 
+        public void ViewContact(Contact contact) {
 
-        public void ViewContact(Contact contact) 
-        {
-        
         }
 
-        public void EditContact(Contact contact) 
-        {
-            
+        public void EditContact(Contact contact) {
+
         }
 
-        public void DeleteContact(Contact contact)
-        {
+        public void DeleteContact(Contact contact) {
 
         }
     }
